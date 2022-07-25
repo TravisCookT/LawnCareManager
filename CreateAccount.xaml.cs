2ac4e003da24f73d6092996ef82504974aad9099
Travis Cook <trey.t.cook@gmail.com>
2022-07-24 22:55:18 -0500

    Add project files.

---
+++ CreateAccount.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
+
+namespace CustomerDashboard
+{
+    /// <summary>
+    /// Interaction logic for CreateAccount.xaml
+    /// </summary>
+    public partial class CreateAccount : Window
+    {
+        public CreateAccount()
+        {
+            InitializeComponent();
+        }
+
+        public void Reset()
+        {
+            txtNewFirstname.Text = "";
+            txtNewLastname.Text = "";
+            txtNewUsername.Text = "";
+            txtNewPassword.Password = "";
+            txtNewUsername.Text = "";
+        }
+
+        private void btnCreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtNewUserEmail.Text.Length == 0)
+            {
+                txtNewUserEmail.Foreground = Brushes.Red;
+
+            }
+            else if (!Regex.IsMatch(txtNewUserEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                txtNewUserEmail.Foreground = Brushes.Red;
+                txtNewUserEmail.Select(0, txtNewUserEmail.Text.Length);
+            }
+            else
+            {
+                string username = txtNewUsername.Text;
+                string firstname = txtNewFirstname.Text;
+                string lastname = txtNewLastname.Text;
+                string email = txtNewUserEmail.Text;
+                string password = txtNewPassword.Password;
+                if(txtNewPassword.Password.Length == 0)
+                {
+                    txtNewPassword.Foreground = Brushes.Red;
+                }
+                else
+                {
+                    SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LawnCareDb;Integrated Security=True;" +
+                        "Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
+
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Insert into dbo.tblUser (UserId,UserFirstName,UserLastName,UserEmail,UserPassword) values('" + username + "','" + firstname + "','" + lastname + "','" + email + "','" + password + "')", con);
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Reset();
+                }
+            }
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            LoginScreen loginScreen = new LoginScreen();
+            loginScreen.Show();
+            this.Close();
+        }
+    }
+}
---
+++ LoginScreen.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+using System.Data;
+using System.Data.SqlClient;
+using CustomerDbGUI;
+
+namespace CustomerDashboard
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LawnCareDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM tblUser WHERE UserId=@UserId AND UserPassword=@UserPassword";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@UserId", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@UserPassword", txtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = sqlCmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                
+                if(count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    //string username = dataSet.Tables[0].Rows[0]["UserFirstName"].ToString() + " " + dataSet.Tables[0].Rows[0]["UserLastName"].ToString();
+                    //dashboard.TextBlockName.Text = username;
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is incorrect");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void btnCreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            CreateAccount createAccount = new CreateAccount();
+            createAccount.Show();
+            this.Close();
+        }
+    }
+}
